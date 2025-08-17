2a4fc03d6c8b98c4b1260f8357b09937c22b0ce2
Vladyslav Boiko <vladyslav.boiko@student.tuke.sk>
2025-08-17 19:13:39 +0200

    final version

--- Project_0/tictactoe/tictactoe.py
+++ Project_0/tictactoe/tictactoe.py
@@ -61,17 +61,14 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    # строки
     for row in board:
         if row[0] == row[1] == row[2] != EMPTY:
             return row[0]
 
-    # столбцы
     for j in range(3):
         if board[0][j] == board[1][j] == board[2][j] != EMPTY:
             return board[0][j]
 
-    # диагонали
     if board[0][0] == board[1][1] == board[2][2] != EMPTY:
         return board[0][0]
     if board[0][2] == board[1][1] == board[2][0] != EMPTY:
